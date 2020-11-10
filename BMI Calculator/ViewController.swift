ff08ae1dc8f4bf4103a5bd6019bfc3dec629ff7e
maixuanlinh <maixuanlinhdkh@gmail.com>
2020-11-10 13:49:47 +0200

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Mai Xuan Linh on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
